811bf38ad2de065a8f09afef600dfc75c8a630a4
Fernando Sepulveda <sepulvedafernando04@gmail.com>
2026-05-21 23:42:24 -0400

    Module 3 dynamic JSON rendering

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,49 +1,10 @@
+const trips = require('../data/trips.json');
+
 const travel = (req, res) => {
     res.render('travel', {
         title: 'Travlr Getaways',
         pageHeader: 'Travel',
-        trips: [
-            {
-                code: 'B0101',
-                name: 'Cancun',
-                length: '4 nights / 5 days',
-                start: 'Feb 14, 2021',
-                resort: 'Emerald Bay, 3-stars',
-                perPerson: '$799.00'
-            },
-            {
-                code: 'B0103',
-                name: 'Barbados',
-                length: '5 nights / 6 days',
-                start: 'Feb 28, 2021',
-                resort: 'Castaway Cove, 4-stars',
-                perPerson: '$1299.00'
-            },
-            {
-                code: 'B0401',
-                name: 'Panama City',
-                length: '4 nights / 5 days',
-                start: 'Mar 21, 2021',
-                resort: 'Sunseeker Surf, 4-stars',
-                perPerson: '$1199.00'
-            },
-            {
-                code: 'B0701',
-                name: 'Tahiti',
-                length: '6 nights / 7 days',
-                start: 'Mar 28, 2021',
-                resort: 'Hedonist Heaven, 5-stars',
-                perPerson: '$1799.00'
-            },
-            {
-                code: 'B0901',
-                name: 'French Riviera',
-                length: '5 nights / 6 days',
-                start: 'Apr 11, 2021',
-                resort: 'Chateau Royal, 5-stars',
-                perPerson: '$2499.00'
-            }
-        ]
+        trips
     });
 };
 
